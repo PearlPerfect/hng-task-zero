30f92a584981945b63b458776d93feeb5375e627
PearlPerfect <zenithyonex4life@gmail.com>
2025-01-30 00:45:56 +0100

    hng-task0

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: 'https://hng12-javascript.vercel.app/', 
+  methods: ['GET'], 
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  const email = "imoudoh2025@gmail.com";
+  const current_datetime = new Date().toISOString(); // ISO 8601 in UTC
+  const github_url = "https://github.com/PearlPerfect/hng-task-zero";
+
+  const response = {
+    email,
+    current_datetime,
+    github_url,
+  };
+
+  res.status(200).json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
